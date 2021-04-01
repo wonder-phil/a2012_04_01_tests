18082c3eb6c47717e4eeb95543844cb5cf40fc39
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2021-04-01 16:18:42 -0400

    Initial commit

--- app/src/androidTest/java/me/pgb/a2021_03_29a_uitesting/InstrumentedTests_1.java
+++ app/src/androidTest/java/me/pgb/a2021_03_29a_uitesting/InstrumentedTests_1.java
@@ -1,5 +1,8 @@
 package me.pgb.a2021_03_29a_uitesting;
 
+import android.app.Activity;
+import android.widget.Button;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +13,9 @@ import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThat;
@@ -44,11 +50,34 @@ public class InstrumentedTests_1 {
     @Test
     public void testActivity_2() {
         try(ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
-            scenario.moveToState(Lifecycle.State.CREATED);    // Moves the activity state to State.RESUMED.
+            scenario.moveToState(Lifecycle.State.DESTROYED);    // Moves the activity state to State.RESUMED.
             scenario.onActivity(activity -> {
                 assertEquals("myButton not null", activity.myButton, null);
             });
         }
     }
 
+    @Test
+    public void testActivty_3() {
+        try(ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
+            scenario.onActivity(activity -> {
+                Button myButton = activity.findViewById(R.id.basic_button);
+                myButton.performClick();
+
+                
+                // Check the Logcat...
+            });
+        }
+    }
+
+    @Test
+    public void testActivty_4() {
+        try(ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class)) {
+            scenario.moveToState(Lifecycle.State.RESUMED);    // Moves the activity state to State.RESUMED.
+            scenario.onActivity(activity -> {
+                //onView(withId(R.id.basic_button)).perform(click());
+                scenario.getResult();
+            });
+        }
+    }
 }
